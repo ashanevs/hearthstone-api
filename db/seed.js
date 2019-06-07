449f9555ddfb3b0b03997280ddb485ce3ea151d9
Shane Vann-Shirley <ashanev@gmail.com>
2019-06-07 08:24:34 -0400

    started work on routes files

--- db/seed.js
+++ db/seed.js
@@ -5,17 +5,6 @@ const setsData = require("./setData.json");
 const Class = require("./models/Class");
 const classData = require("./classData.json");
 
-// Card.find({}).remove(() =>
-//   Card.create(cardsData).then(res => console.log(res))
-// );
-
-// theSet.find({})
-//   .remove(() => theSet.create(setsData).then(res => console.log(res)));
-
-// Class.find({}).remove(() =>
-//   Class.create(classData).then(res => console.log(res))
-// );
-
 const validCardData = cardsData.map(card => {
   const cardCopy = JSON.parse(JSON.stringify(card));
   delete cardCopy.cardSet;
@@ -23,21 +12,71 @@ const validCardData = cardsData.map(card => {
   return cardCopy;
 });
 
+const validSetData = setsData.map(set => {
+  const setCopy = JSON.parse(JSON.stringify(set));
+  delete setCopy.cards;
+  return setCopy;
+});
+
+const validClassData = classData.map(theClass => {
+  const classCopy = JSON.parse(JSON.stringify(theClass));
+  delete classCopy.cards;
+  return classCopy;
+});
+
+// Card.deleteMany({}).then(_ => {
+//   Card.create(validCardData).then(cardDocs => {
+
+// cardDocs.forEach((cardFromCollection, cardIndex) => {
+//   const cardFromJson = cardsData.find(card => {
+//     return card.name === cardFromCollection.name;
+//   });
+//   console.log(cardFromCollection);
+//   console.log(cardFromJson);
+// });
+
+//     const cardFromCollection = cardDocs[0];
+//     const cardFromJson = cardsData.find(card => {
+//       return card.name === cardFromCollection.name;
+//     });
+//     console.log(cardFromCollection);
+//     console.log(cardFromJson);
+//   });
+// });
+
 Card.deleteMany({}).then(_ => {
   Card.create(validCardData).then(cardDocs => {
-    // cardDocs.forEach((cardFromCollection, cardIndex) => {
-    //   const cardFromJson = cardsData.find(card => {
-    //     return card.name === cardFromCollection.name;
-    //   });
-    //   console.log(cardFromCollection);
-    //   console.log(cardFromJson);
-    // });
-    const cardFromCollection = cardDocs[0];
-    const cardFromJson = cardsData.find(card => {
-      return card.name === cardFromCollection.name;
+    Class.deleteMany({}).then(_ => {
+      Class.create(validClassData).then(classDocs => {
+        theSet.deleteMany({}).then(_ => {
+          theSet.create(validSetData).then(setDocs => {
+            for (let i = 0; i < cardDocs.length; i++) {
+              const cardFromCollection = cardDocs[i];
+              const cardFromJson = cardsData.find(card => {
+                return card.name === cardFromCollection.name;
+              });
+              Class.findOne({ name: cardFromJson.playerClass })
+                .then(oneDoc => {
+                  oneDoc.cards.push(cardFromCollection._id);
+                  cardFromCollection.playerClass = oneDoc._id;
+                  cardFromCollection.save();
+                  oneDoc.save();
+                })
+                .then(_ => {
+                  theSet
+                    .findOne({ name: cardFromJson.cardSet })
+                    .then(oneSet => {
+                      oneSet.cards.push(cardFromCollection._id);
+                      cardFromCollection.cardSet = oneSet._id;
+                      cardFromCollection.save();
+                      oneSet.save();
+                    });
+                });
+            }
+          });
+        });
+      });
     });
-    console.log(cardFromCollection);
-    console.log(cardFromJson);
   });
 });
 
